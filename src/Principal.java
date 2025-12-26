fa44ff81e0e1ef6d7663e7bda2f8ae243a87da14
nunesma <nunes.ma@ufs.br>
2025-12-26 16:03:52 -0300

    Nova atualizacao da classe Principal

---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Boas vindas ao Sistema de Gestao Escolar!");
+
+        String nome = "Rodrigo da Silva";
+        int idade = 35;
+        double nota = 8.5;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(nota);
+
+        nota = 9.0;
+
+        System.out.println(nota);
+
+        boolean passouNaProva = true;
+
+        System.out.println(passouNaProva);
+    }
+}
