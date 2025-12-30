c93f370ce703f47197bbce02ec9c5e115d21ca17
nunesma <nunes.ma@ufs.br>
2025-12-29 23:37:37 -0300

    Atualizando a classe Estudantes

--- src/Estudante.java
+++ src/Estudante.java
@@ -1,7 +1,31 @@
 public class Estudante {
-    String nome;
-    String telefone;
-    String endereco;
-    String responsavel;
+
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String resposavel;
+
+    public Estudante(String nome, String telefone, String endereco, String resposavel) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.resposavel = resposavel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getResposavel() {
+        return resposavel;
+    }
 
 }
